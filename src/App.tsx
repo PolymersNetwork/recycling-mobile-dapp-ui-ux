941790e973b6ce4f69e7d7b223366f008a322d1f
Polymers <polymers.network@gmail.com>
2025-08-28 09:09:37 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { BottomNav } from "@/components/mobile/BottomNav";
+import { MobileHeader } from "@/components/mobile/MobileHeader";
+
+import { RecyclingProvider } from "@/contexts/RecyclingContext";
+
 import { Start } from "@/pages/Start";
 import { Home } from "@/pages/mobile/Home";
 import { Scan } from "@/pages/mobile/Scan";
@@ -11,7 +17,8 @@ import { Projects } from "@/pages/mobile/Projects";
 import { Portfolio } from "@/pages/mobile/Portfolio";
 import { Profile } from "@/pages/mobile/Profile";
 import { Marketplace } from "@/pages/mobile/Marketplace";
-import NotFound from "./pages/NotFound";
+import { RecycleScreen } from "@/pages/mobile/RecycleScreen";
+import NotFound from "@/pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -20,21 +27,29 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <div className="relative">
-          <Routes>
-            <Route path="/start" element={<Start />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/scan" element={<Scan />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/marketplace" element={<Marketplace />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <BottomNav />
-        </div>
-      </BrowserRouter>
+      <RecyclingProvider>
+        <BrowserRouter>
+          <div className="relative min-h-screen pb-16 bg-background">
+            {/* MobileHeader can be dynamic based on route */}
+            <MobileHeader title="Polymers" showNotifications showSettings />
+
+            <Routes>
+              <Route path="/start" element={<Start />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/scan" element={<Scan />} />
+              <Route path="/recycle" element={<RecycleScreen />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/marketplace" element={<Marketplace />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+
+            {/* Persistent Bottom Navigation */}
+            <BottomNav />
+          </div>
+        </BrowserRouter>
+      </RecyclingProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
